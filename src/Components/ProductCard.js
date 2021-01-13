bee904430e14bdc9f12f9866f65a3ddf3cf528dd
Ujjwal Gautam <ujm2099@gmail.com>
2021-01-14 00:57:51 +0530

    Completed the basic UI of product page

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,4 +1,12 @@
-import { Grid, Button, Paper, Typography, Box } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Grid,
+  Button,
+  Paper,
+  Typography,
+  Box,
+  TextField,
+} from '@material-ui/core';
 import '../Styles/ProductCard.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../theme';
@@ -9,15 +17,22 @@ import UpvoteIcon from '../images/Upvote.svg';
 import BucketIcon from '../images/Bucket.svg';
 import DownloadIcon from '../images/Download.svg';
 import Avatar from '@material-ui/core/Avatar';
+
 //import { Link } from 'react-router-dom';
 
 function ProductCard(props) {
+  const [comment, setComment] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let body = JSON.stringify(comment);
+    console.log(body);
+  };
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <Grid container justify="center" direction="row" spacing={20}>
+        <Grid container justify="center" direction="row" spacing={10}>
           <Grid item>
-            <Grid container justify="flex-start" direction="column" spacing={2}>
+            <Grid container justify="flex-start" direction="column" spacing={1}>
               <Grid item>
                 <Button
                   variant="contained"
@@ -53,11 +68,16 @@ function ProductCard(props) {
           </Grid>
           <Grid item>
             <Box my={0}>
-              <Grid container direction="column" justify="start">
+              <Grid
+                container
+                direction="column"
+                justify="flex-start"
+                spacing={2}
+              >
                 <Grid item>
                   <Paper
                     variant="elevation"
-                    elevation={5}
+                    elevation={1}
                     className="product-desc"
                   >
                     <Grid item>
@@ -115,6 +135,7 @@ function ProductCard(props) {
                   >
                     <Grid item>
                       <Paper
+                        elevation={0}
                         style={{
                           color: '#8693AE',
                           backgroundColor: '#F0F6FF',
@@ -140,6 +161,7 @@ function ProductCard(props) {
                     </Grid>
                     <Grid item>
                       <Paper
+                        elevation={0}
                         style={{
                           color: '#8693AE',
                           backgroundColor: '#F0F6FF',
@@ -165,11 +187,135 @@ function ProductCard(props) {
                     </Grid>
                   </Grid>
                 </Grid>
+                <Grid item>
+                  <Grid container direction="row" justify="center">
+                    <Paper
+                      variant="elevation"
+                      elevation={4}
+                      style={{ width: '80%' }}
+                    >
+                      <Grid
+                        container
+                        direction="column"
+                        justify="flex-start"
+                        style={{ padding: '8px' }}
+                      >
+                        <Grid item>
+                          <Box display="flex" flexDirection="row">
+                            <Avatar
+                              style={{
+                                height: '30px',
+                                width: '30px',
+                              }}
+                            >
+                              {props.username[0]}
+                            </Avatar>
+                            &nbsp;
+                            <Box mt={1}>
+                              <strong>{props.username}</strong>
+                            </Box>
+                          </Box>
+                        </Grid>
+                        <Grid item>
+                          <form onSubmit={handleSubmit}>
+                            <Box display="flex">
+                              <Box flexGrow={1} pl={4} pr={2}>
+                                <TextField
+                                  type="text"
+                                  placeholder="Write a comment...."
+                                  name="comment"
+                                  variant="outlined"
+                                  fullWidth
+                                  margin="dense"
+                                  value={comment}
+                                  onChange={(e) => {
+                                    setComment(e.target.value);
+                                  }}
+                                />
+                              </Box>
+                            </Box>
+                            <Box display="flex" flexDirection="row-reverse">
+                              <Button type="submit">
+                                <Typography color="primary">POST</Typography>
+                              </Button>
+                            </Box>
+                          </form>
+                        </Grid>
+                      </Grid>
+                    </Paper>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  {props.comments.map((comment, index) => (
+                    <Box
+                      key={index}
+                      display="flex"
+                      flexDirection="column"
+                      pt={1}
+                    >
+                      <Grid item>
+                        <Grid container direction="row" justify="center">
+                          <Paper
+                            variant="elevation"
+                            elevation={0}
+                            style={{
+                              width: '80%',
+                              color: '#8693AE',
+                              backgroundColor: '#F0F6FF',
+                            }}
+                          >
+                            <Grid
+                              container
+                              direction="column"
+                              justify="flex-start"
+                              style={{ padding: '8px' }}
+                            >
+                              <Grid item>
+                                <Box display="flex" flexDirection="row">
+                                  <Avatar
+                                    style={{
+                                      height: '30px',
+                                      width: '30px',
+                                    }}
+                                  >
+                                    {comment.username[0]}
+                                  </Avatar>
+                                  &nbsp;
+                                  <Box mt={1} style={{ fontSize: '14px' }}>
+                                    <strong>{comment.username}</strong>
+                                  </Box>
+                                </Box>
+                              </Grid>
+                              <Grid item>
+                                <Box display="flex">
+                                  <Box flexGrow={1} pl={4} pr={2}>
+                                    <Typography
+                                      style={{
+                                        color: 'black',
+                                        fontSize: '12px',
+                                      }}
+                                    >
+                                      {comment.value}
+                                    </Typography>
+                                  </Box>
+                                </Box>
+                                <Box pl={4} pr={2} style={{ fontSize: '8px' }}>
+                                  {comment.time}
+                                </Box>
+                              </Grid>
+                            </Grid>
+                          </Paper>
+                        </Grid>
+                      </Grid>
+                    </Box>
+                  ))}
+                </Grid>
               </Grid>
             </Box>
           </Grid>
           <Grid item>
             <Paper
+              elevation={0}
               style={{
                 color: '#8693AE',
                 backgroundColor: '#F0F6FF',
@@ -189,11 +335,11 @@ function ProductCard(props) {
                   >
                     <Grid item>
                       <Avatar style={{ height: '30px', width: '30px' }}>
-                        U
+                        {props.owner[0]}
                       </Avatar>
                     </Grid>
                     <Grid item>
-                      <Box mt={1}>User Name</Box>
+                      <Box mt={1}>{props.owner}</Box>
                     </Grid>
                   </Grid>
                 </Grid>
--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -3,24 +3,62 @@ import Navbar from '../Components/Navbar';
 import { ThemeProvider } from '@material-ui/core/styles';
 import ProductCard from '../Components/ProductCard';
 import theme from '../theme';
+import { Box } from '@material-ui/core';
 
 function ProductPage(props) {
   return (
     <>
-      <Navbar register={true} disablePost={true} />
-      <ThemeProvider theme={theme}>
-        <ProductCard
-          productname={"BYJU'S"}
-          category={'FINTECH'}
-          oneline={'Indian education technology and education firm'}
-          description={
-            ' The Learning App is an Indian educational technology (edtech) and online tutoring firm founded in 2011 by Byju Raveendran. The company with a valuation of $11.1 billion.'
-          }
-          website={'www.byjus.com'}
-          quantity={255}
-          applink={'www.byjus.com/applink'}
-        />
-      </ThemeProvider>
+      <Navbar register={true} disablePost={false} />
+      <Box mt={7}>
+        <ThemeProvider theme={theme}>
+          <ProductCard
+            productname={"BYJU'S"}
+            category={'FINTECH'}
+            oneline={'Indian education technology and education firm'}
+            description={
+              ' The Learning App is an Indian educational technology (edtech) and online tutoring firm founded in 2011 by Byju Raveendran. The company with a valuation of $11.1 billion.'
+            }
+            website={'www.byjus.com'}
+            quantity={255}
+            owner={'Nitish Kumar'}
+            username={'Krishan Kumar Pareek'}
+            applink={'www.byjus.com/applink'}
+            comments={[
+              {
+                value: 'This is very good.',
+                username: 'Priyam Patel',
+                time: '12 mins ago',
+              },
+              {
+                value: 'Really nice to use',
+                username: 'Santosh Kumar',
+                time: '15 mins ago',
+              },
+              {
+                value: 'Excellent product',
+                username: 'Rahul Kumar',
+                time: '18 mins ago',
+              },
+              {
+                value: 'Amazing experience',
+                username: 'Abhijeet Verma',
+                time: '28 mins ago',
+              },
+              {
+                value: 'Average',
+                username: 'Vikram Kumar',
+                time: '38 mins ago',
+              },
+              {
+                value: 'wow!! incredible',
+                username: 'Sarvajeet Kumar',
+                time: '46 mins ago',
+              },
+            ]}
+            arr={[1, 2, 3, 4, 5]}
+          />
+        </ThemeProvider>
+      </Box>
     </>
   );
 }
