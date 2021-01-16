d7bff78b43c66eb5bdecfca212c1b1bb8ae36f0a
Ujjwal Gautam <ujm2099@gmail.com>
2021-01-16 16:04:55 +0530

    added admin page

--- src/Components/SignInCard.js
+++ src/Components/SignInCard.js
@@ -28,7 +28,6 @@ function SignInCard() {
     axios
       .post('https://jsonplaceholder.typicode.com/posts', values)
       .then((res) => {
-        localStorage.setItem(res.)
         console.log(res);
       })
       .catch((err) => {
